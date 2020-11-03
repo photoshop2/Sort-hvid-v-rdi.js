2418bb6293c1e5277b7cf7a6dff7c24e67058a70
fredyghety <73481525+fredyghety@users.noreply.github.com>
2020-11-03 11:00:31 +0100

    Add files via upload

--- importfile.js
+++ importfile.js
@@ -3,39 +3,31 @@ var vid;
 var pix;
 var but;
 var cam;
+var showing=[false,false,false];
 var fil;
 var x=100;
 var y=100;
 
+
 function setup() {
+  //sætter programmet op med to knapper. en der kan loade filer og en der kan tage kameraet
+  //false værdien betyder at den kun modtager 1 fil som argument
+
     createCanvas(x, y);
     fil=createFileInput(handleFile,false);
-    
-    
     but=createButton("kamera");
     but.mousePressed(kamera);
     pixelDensity(1);
-    
-    
   }
 
 
+  //funktonen der bliver kaldt når man har valgt en fil
   function handleFile(file) {
-    
-    console.log("hejsa: "+cam);
-    if(vid!=null){
-    vid=null;
-    vid.hide();
-    
-    }
-    source=null;
-    if(cam!=null){
-      cam.hide();
-      cam=null;
-      console.log("gemmer kameraet");
-    }
 
-    print(file);
+    
+    source=null;
+
+    //hvis filen man har valgt er et billede
     if (file.type === 'image') {
       source = createImg(file.data, '');
       source.hide();
@@ -44,62 +36,117 @@ function setup() {
       console.log("det et billede");
     
       createCanvas(x, y);
-      image(source,0,0,width,height);
-      loadpic();
+     
+      
+      showing=[true,false,false];
+      
     
     }
+    //hvis filen man har valgt er en video
       else if (file.type === 'video') {
         console.log("det en video");
         vid=createVideo(file.data);
-        vid.show();
+        source=vid;
+        vid.hide();
         vid.loop();
-        loadvid();
-
         
-        x=0;
-        y=0;
+
+        createCanvas(vid.width, vid.height);
+
+        showing=[false,false,true];
+        
+    }
     
-        createCanvas(x, y);
-    }
      else {
-        //hvis sourcen ikke er et billede
-        console.log("indsæt et billede eller en video din dumrian");
+        //hvis filen ikke er et billede
+        
       source = null;
+      console.log("vælg en fil der ikke er garb");
     }
+
+   
   }
 
 function draw(){
     
+
+    //der er 3 værdier i arryen "showing". værdierne beskriver hvilken filtype der arbejdes med
+    //arrayen er lavet af 3 bools. den første står for billede. den anden står for kamera og den tredje står for video
+    //[billede,kamera,video]
+
     background(57);
     
-    if(source!=null){
+    //tegner billedet
+    if(showing[0]){
+    x=source.width;
+    y=source.height;
+    createCanvas(x,y);
     image(source,0,0,width,height);
+    
     }
 
+    //tegner kameraet 
+    if(showing[1]){
+      x=cam.width;
+      y=cam.height;
+      createCanvas(x,y);
+      cam.loadPixels();
+      loadPixels();
+  for (var y = 0; y < cam.height; y++) {
+    for (var x = 0; x < cam.width; x++) {
+      var index = ((x+y*width)*4);
+      
+      var r = cam.pixels[index + 0];
+      var g = cam.pixels[index + 1];
+      var b = cam.pixels[index + 2];
 
+      pixels[index + 0]=r;
+      pixels[index + 1]=g;
+      pixels[index + 2]=b;
+      pixels[index + 3]=255;
+      
+    }
+  }
+  updatePixels();
+    }
+
+//tegner video
+    if(showing[2]){
+      x=vid.width;
+      y=vid.height;
+      createCanvas(x,y);
+      vid.loadPixels();
+      loadPixels();
+  for (var y = 0; y < vid.height; y++) {
+    for (var x = 0; x < vid.width; x++) {
+      var index = ((x+y*width)*4);
+      
+      var r = vid.pixels[index + 0];
+      var g = vid.pixels[index + 1];
+      var b = vid.pixels[index + 2];
+
+      pixels[index + 0]=r;
+      pixels[index + 1]=g;
+      pixels[index + 2]=b;
+      pixels[index + 3]=255;
+    }
+  }
+  updatePixels();
+    }
+    
 
 
 }
 
-
+//når man trykker på kamera knappen bliver denne finktion kaldet
 function kamera(){
-  if(cam==null){
-  cam=createCapture();
-  }
-  if(vid!=null){
-  vid.hide();
+  fil.value('');
   vid=null;
-  }
-
   source=null;
-  //returner captured her
+  cam=createCapture();
+  source=cam;
+  cam.hide();
+  showing=[false,true,false];
+
 }
 
-
-function loadvid(){
-    //returnere den video man har valgt som filtype så man kan lave kloge ting
-}
-
-function loadpic(){
-  //returnere det billede man har valgt
-}
